21746cd0a9850e7a5c142a4630b50f7954abd65d
bojanS94 <62501546+bojanS94@users.noreply.github.com>
2021-06-06 09:45:34 +0200

    Project finished 6.6.2021

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
 'use strict';
-
+//Variables
 const checkBtn = document.querySelector('.check');
+let titleTxt = document.querySelector('.titleText');
 let guessMsg = document.querySelector('.message');
 let scoreTxt = document.querySelector('.score');
 let guessInput = document.querySelector('.guess');
+let numberReveal = document.querySelector('.numberReveal');
+let hiddenNum = document.querySelector('.number');
 const againBtn = document.querySelector('.again');
 let score = 20;
 let highScore = 0;
+//Display message function
+const displayMessage = (message) => {
+    guessMsg.textContent = message;
+}
 //Calc functions
 let scoreCalcWrong = () => {
     score--;
     scoreTxt.textContent = score;
     if (score === 0) {
-        scoreTxt.textContent = "💥 GAME OVER! 💥";
+        titleTxt.textContent = "💥 GAME OVER! 💥";
+        numberReveal.textContent = "The humber was:";
+        hiddenNum.textContent = randomNum;
         score = 0;
-        checkBtn.setAttribute("disabled", false);
+        checkBtn.setAttribute("disabled", true);
+        guessInput.setAttribute("disabled", true);
+        guessInput.value = '';
         document.querySelector('body').classList.add('gameover');
     }
 }
 let scoreCalcRight = () => {
     score++;
     scoreTxt.textContent = score;
+    displayMessage('🎉 Correct!');
+    checkBtn.setAttribute("disabled", true);
+    document.querySelector('body').classList.add('gamewin');
+    titleTxt.textContent = "👍 GOOD JOB!";
+    hiddenNum.textContent = randomNum;
+    if (score > highScore) {
+        highScore = score;
+        document.querySelector('.highscore').textContent = highScore;
+    }
 }
 againBtn.addEventListener('click', () => {
     guessInput.value = '';
@@ -28,29 +48,21 @@ againBtn.addEventListener('click', () => {
 });
 //Random number
 const randomNum = Math.trunc(Math.random() * 20) + 1;
-
+//Check secret number button
 checkBtn.addEventListener('click', () => {
     const guessInput = Number(document.querySelector('.guess').value);
-
     if (!guessInput) {
-        guessMsg.textContent = "⛔ There is no number!";
+        displayMessage('⛔ There is no number!');
     }
     else if (guessInput === randomNum) {
-        guessMsg.textContent = "🎉 Correct!";
         scoreCalcRight();
-        checkBtn.setAttribute("disabled", false);
-        document.querySelector('body').classList.add('gamewin');
-        if (score > highScore) {
-            highScore = score;
-            document.querySelector('.highscore').textContent = highScore;
-        }
     }
     else if (guessInput > randomNum) {
-        guessMsg.textContent = "⬇ Lower 😋";
         scoreCalcWrong();
+        displayMessage('⬇ Lower 😋');
     }
     else if (guessInput < randomNum) {
-        guessMsg.textContent = "⬆ Higher 🥵";
         scoreCalcWrong();
+        displayMessage('⬆ Higher 🥵');
     }
 });
